a761cebdda8ce0d26ee73e45523327c2b0396b67
Dennis Nowicki <dennis.nowicki@utlook.com>
2020-01-27 11:31:58 +0100

    Added basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { Routes, RouterModule } from '@angular/router' ;
+
+const routes: Routes = [
+  { path: '' , redirectTo: 'home' , pathMatch: 'full' },
+  { path: 'home' , component: HomeComponent },
+  { path: 'about' , component: FavoritesComponent},
+  { path: 'contact' , component: ContactComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FavoritesComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
